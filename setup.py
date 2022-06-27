767ecbc32cf71ca381da8cbdec4d12a158baa64c
Rachael Kiai <rachaelnjerikiai1@gmail.com>
2022-06-27 12:54:43 +0300

    Python game

---
+++ setup.py
@@ -0,0 +1,16 @@
+import pygame
+pygame.init()
+surface = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Helicopter')
+clock = pygame.time.Clock()
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+#    update specific things instead of the whole window
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
+quit()
